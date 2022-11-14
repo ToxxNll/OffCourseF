b6ddd29b4c113be245feabc1b7d399e8b971964f
ToxxNll <dolayevtohtar@gmail.com>
2022-11-14 12:11:45 +0600

    Welcome page
    Creating welcome page when app starts

---
+++ offcourse/lib/pages/welcome_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 3,
+        itemBuilder: ((context, index){
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+          );
+        }),
+      );
+    )
+  }
+}
