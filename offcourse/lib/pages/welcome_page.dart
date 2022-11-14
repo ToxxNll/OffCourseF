8356d749a26a1cfd8db45880eaa17efc0a8d5dd2
ToxxNll <dolayevtohtar@gmail.com>
2022-11-14 19:45:13 +0600

    Adding navigation buttons

--- offcourse/lib/pages/welcome_page.dart
+++ offcourse/lib/pages/welcome_page.dart
@@ -8,24 +8,95 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-  List images = ["welcomeImg.png"];
+  List images = ["welcomeImg.png", "welcomeImg.png", "welcomeImg.png"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView.builder(
-      scrollDirection: Axis.vertical,
-      itemCount: images.length,
-      itemBuilder: ((context, index) {
-        return Container(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("img/" + images[index]),
-                  fit: BoxFit.cover)),
-        );
-      }),
-    ));
+      // bottomNavigationBar: Row(children: [
+      //   Column(
+      //     children: const [
+      //       OutlinedButton(
+      //           onPressed: null,
+      //           child: Text(
+      //             "Зарегистрироваться",
+      //             style: TextStyle(
+      //               color: Colors.black54,
+      //             ),
+      //           )),
+      //     ],
+      //   ),
+      // ]),
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: ((context, index) {
+          return Container(
+              alignment: Alignment.bottomCenter,
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("img/" + images[index]),
+                      fit: BoxFit.cover)),
+              child: Visibility(
+                  visible: (index == images.length - 1),
+                  child: Container(
+                    height: MediaQuery.of(context).size.width * 0.20,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: MediaQuery.of(context).size.width * 0.2,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Каталог"),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(left: 8.0),
+                            height: MediaQuery.of(context).size.width * 0.2,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Войти"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ))
+              // Container(
+              //     child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.center,
+              //         children: [
+              //       Column(children: const [
+              //         OutlinedButton(
+              //             onPressed: null,
+              //             child: Text(
+              //               "Каталог курсов",
+              //               style: TextStyle(
+              //                 color: Colors.black54,
+              //               ),
+              //             ))
+              //       ]),
+              //       Column(
+              //         children: const [
+              //           OutlinedButton(
+              //               onPressed: null,
+              //               child: Text(
+              //                 "Зарегистрироваться",
+              //                 style: TextStyle(
+              //                   color: Colors.black54,
+              //                 ),
+              //               )),
+              //         ],
+              //       )
+              //     ]))
+              );
+        }),
+      ),
+    );
   }
 }
