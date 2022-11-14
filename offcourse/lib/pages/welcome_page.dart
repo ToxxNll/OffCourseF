4cc19bea7f3533e8917910205a3431a3ebd155a4
ToxxNll <dolayevtohtar@gmail.com>
2022-11-14 13:18:14 +0600

    First img in WelcomeP

--- offcourse/lib/pages/welcome_page.dart
+++ offcourse/lib/pages/welcome_page.dart
@@ -8,19 +8,24 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images = ["welcomeImg.png"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: 3,
-        itemBuilder: ((context, index){
-          return Container(
-            width: double.maxFinite,
-            height: double.maxFinite,
-          );
-        }),
-      );
-    )
+        body: PageView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: images.length,
+      itemBuilder: ((context, index) {
+        return Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("img/" + images[index]),
+                  fit: BoxFit.cover)),
+        );
+      }),
+    ));
   }
 }
