4a45cc9769cf7f213356a1f3ec75b6452095ea1e
ToxxNll <dolayevtohtar@gmail.com>
2022-11-21 16:55:06 +0600

    Update welcome_page.dart

--- offcourse/lib/pages/welcome_page.dart
+++ offcourse/lib/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:offcourse/widgets/app_text.dart';
 import 'package:offcourse/widgets/applarge_text.dart';
 import 'package:offcourse/widgets/responsive_button.dart';
 
@@ -17,99 +18,68 @@ class _WelcomePageState extends State<WelcomePage> {
     var deviceSize = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      // bottomNavigationBar: Row(children: [
-      //   Column(
-      //     children: const [
-      //       OutlinedButton(
-      //           onPressed: null,
-      //           child: Text(
-      //             "Зарегистрироваться",
-      //             style: TextStyle(
-      //               color: Colors.black54,
-      //             ),
-      //           )),
-      //     ],
-      //   ),
-      // ]),
       body: PageView.builder(
         scrollDirection: Axis.vertical,
         itemCount: images.length,
         itemBuilder: ((context, index) {
-          return Container(
-              alignment: Alignment.bottomCenter,
-              width: double.maxFinite,
-              height: double.maxFinite,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("img/" + images[index]),
-                      fit: BoxFit.cover)),
-              child: Visibility(
-                  visible: (index == images.length - 1),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.08,
-                    child: Row(
+          return Stack(children: <Widget>[
+            Container(
+                alignment: Alignment.topCenter,
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: Container(
-                              height: MediaQuery.of(context).size.height * 0.8,
-                              child: ResponsiveButton(
-                                width: MediaQuery.of(context).size.width * 0.20,
-                                height:
-                                    MediaQuery.of(context).size.width * 0.20,
-                                text: "Каталог",
-                              )
-                              // child: ElevatedButton(
-                              //   onPressed: () {},
-                              //   child: AppLargreText(
-                              //     text: "Каталог",
-                              //     color: Colors.white,
-                              //   ),
-                              // ),
-                              ),
-                        ),
-                        Expanded(
+                        AppLargreText(text: "Welcome to OffCourse"),
+                        AppText(
+                            text:
+                                "Here you can find Loreum ipsium asdasdjn kjasndkansd kjsad")
+                      ],
+                    )
+                  ],
+                )),
+            Container(
+                alignment: Alignment.bottomCenter,
+                width: double.maxFinite,
+                height: double.maxFinite,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("img/" + images[index]),
+                        fit: BoxFit.cover)),
+                child: Visibility(
+                    visible: (index == images.length - 1),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      child: Row(
+                        children: [
+                          Expanded(
                             child: Container(
                                 height:
-                                    MediaQuery.of(context).size.height * 0.1,
+                                    MediaQuery.of(context).size.height * 0.8,
                                 child: ResponsiveButton(
                                   width:
                                       MediaQuery.of(context).size.width * 0.20,
                                   height:
                                       MediaQuery.of(context).size.width * 0.20,
-                                  text: "Войти",
-                                )))
-                      ],
-                    ),
-                  ))
-
-              // Container(
-              //     child: Row(
-              //         mainAxisAlignment: MainAxisAlignment.center,
-              //         children: [
-              //       Column(children: const [
-              //         OutlinedButton(
-              //             onPressed: null,
-              //             child: Text(
-              //               "Каталог курсов",
-              //               style: TextStyle(
-              //                 color: Colors.black54,
-              //               ),
-              //             ))
-              //       ]),
-              //       Column(
-              //         children: const [
-              //           OutlinedButton(
-              //               onPressed: null,
-              //               child: Text(
-              //                 "Зарегистрироваться",
-              //                 style: TextStyle(
-              //                   color: Colors.black54,
-              //                 ),
-              //               )),
-              //         ],
-              //       )
-              //     ]))
-              );
+                                  text: "Каталог",
+                                )),
+                          ),
+                          Expanded(
+                              child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.1,
+                                  child: ResponsiveButton(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.20,
+                                    height: MediaQuery.of(context).size.width *
+                                        0.20,
+                                    text: "Войти",
+                                  )))
+                        ],
+                      ),
+                    )))
+          ]);
         }),
       ),
     );
