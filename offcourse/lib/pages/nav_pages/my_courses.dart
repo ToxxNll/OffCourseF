4dc6366b3d6aef36e8c5897cef0cef2757ea4ca5
ToxxNll <dolayevtohtar@gmail.com>
2022-11-28 17:33:10 +0600

    Update my_courses.dart

--- offcourse/lib/pages/nav_pages/my_courses.dart
+++ offcourse/lib/pages/nav_pages/my_courses.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class MyCoursesPage extends StatefulWidget {
+  const MyCoursesPage({super.key});
+
+  @override
+  State<MyCoursesPage> createState() => _MyCoursesState();
+}
+
+class _MyCoursesState extends State<MyCoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
