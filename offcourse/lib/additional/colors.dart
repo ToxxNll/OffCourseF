1f9101ae53eff90e1bad773a304524195cca6f21
ToxxNll <dolayevtohtar@gmail.com>
2022-11-28 17:32:50 +0600

    Update colors.dart

--- offcourse/lib/additional/colors.dart
+++ offcourse/lib/additional/colors.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final Color mainColor = Color(0x00F05D40);
+  static final mainColor = Color.fromARGB(0, 17, 22, 158);
   static final Color white = Color(0x00FFFFFF);
   static final Color black = Color(0x00000000);
   static final Color pressableText = Color(0x00FAC043);
