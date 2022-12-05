8a6b6098a23728eeafa36f7d19b4cac980b3cfc5
ToxxNll <dolayevtohtar@gmail.com>
2022-12-05 16:27:40 +0600

    Update catalog_page.dart

--- offcourse/lib/pages/nav_pages/catalog_page.dart
+++ offcourse/lib/pages/nav_pages/catalog_page.dart
@@ -1,1 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:offcourse/widgets/course_page.dart';
 
+import '../../additional/colors.dart';
+
+class CatalogPage extends StatefulWidget {
+  @override
+  _CatalogPageState createState() => _CatalogPageState();
+}
+
+class _CatalogPageState extends State<CatalogPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          onPressed: () {},
+        ),
+        title: Text('cook',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                color: Color(0xFF545D68))),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 20.0),
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Text('Categories',
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 15.0),
+          TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: AppColors.mainColor,
+              isScrollable: true,
+              labelPadding: EdgeInsets.only(right: 45.0),
+              unselectedLabelColor: Color(0xFFCDCDCD),
+              tabs: [
+                Tab(
+                  child: Text('Course Sample',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                ),
+                Tab(
+                  child: Text('Course Sample',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                ),
+                Tab(
+                  child: Text('Course Sample',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                )
+              ]),
+          Container(
+              height: MediaQuery.of(context).size.height - 50.0,
+              width: double.infinity,
+              child: TabBarView(controller: _tabController, children: [
+                CoursePage(),
+                CoursePage(),
+                CoursePage(),
+              ]))
+        ],
+      ),
+    );
+  }
+}
