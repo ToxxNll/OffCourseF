34113f852659b141d23341d33ff9e50521e38aea
ToxxNll <dolayevtohtar@gmail.com>
2022-12-05 16:28:41 +0600

    Create course_details.dart

---
+++ offcourse/lib/widgets/course_details.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:offcourse/additional/colors.dart';
+
+class CourseDetail extends StatelessWidget {
+  final assetPath, teacher, name;
+
+  CourseDetail({this.assetPath, this.teacher, this.name});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Pickup',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                color: Color(0xFF545D68))),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(children: [
+        SizedBox(height: 15.0),
+        Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: Text('',
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFF17532))),
+        ),
+        SizedBox(height: 15.0),
+        Hero(
+            tag: name,
+            child: Image.asset(assetPath,
+                height: 150.0, width: 100.0, fit: BoxFit.contain)),
+        SizedBox(height: 20.0),
+        Center(
+          child: Text(teacher,
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.mainColor)),
+        ),
+        SizedBox(height: 10.0),
+        Center(
+          child: Text(name,
+              style: TextStyle(
+                  color: Color(0xFF575E67),
+                  fontFamily: 'Varela',
+                  fontSize: 24.0)),
+        ),
+        SizedBox(height: 20.0),
+        Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width - 50.0,
+            child: Text(
+                'An excellent course that we recommend everyone to take. You will gain a ton of knowledge and also strengthen the existing knowledge.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Varela',
+                    fontSize: 16.0,
+                    color: Color(0xFFB4B8B9))),
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Center(
+            child: Container(
+                width: MediaQuery.of(context).size.width - 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    color: AppColors.mainColor),
+                child: Center(
+                    child: Text(
+                  'Add to cart',
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ))))
+      ]),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   backgroundColor: Color(0xFFF17532),
+      //   child: Icon(Icons.fastfood),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
