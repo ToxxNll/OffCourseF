1067b9cd7c1a360c2d36c4864fc2677e2759412f
ToxxNll <dolayevtohtar@gmail.com>
2022-12-05 16:27:46 +0600

    Update profile.dart

--- offcourse/lib/pages/nav_pages/profile.dart
+++ offcourse/lib/pages/nav_pages/profile.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:offcourse/additional/colors.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,11 +13,31 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hello! " + user.email!),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: AppColors.mainColor,
+              child: Text(
+                "Sign Out",
+                style: TextStyle(color: AppColors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
